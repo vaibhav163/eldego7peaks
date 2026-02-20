915b518a54c0b6d70c6cd418ee0f08de9f4e2ece
vaibhav <vaibhavsinghrajput456@gmai.com>
2026-02-20 23:51:40 +0530

    fix vite config + static deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// export default defineConfig({
-//   plugins: [react()],
-// });
-export default {
-  base: './',
-};
+export default defineConfig({
+  plugins: [react()],
+  base: './',   // STATIC deploy works on Vercel
+});
